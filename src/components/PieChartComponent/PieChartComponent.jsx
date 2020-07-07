f3a8ad37ffd2c5513a1bbfef461d1fe181260dc2
Juan Pablo Calao <jcalaop@gmail.com>
2020-07-07 01:40:33 -0500

    add chart styles to pie chart component

--- src/components/PieChartComponent/PieChartComponent.jsx
+++ src/components/PieChartComponent/PieChartComponent.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Pie } from 'react-chartjs-2'
+import styles from '../../scss/components/Chart.module.scss'
 
-export function PieChartComponent({ data }) {
-	return <Pie data={data} />
+export function PieChartComponent({ data, title }) {
+	return (
+		<div className={styles['Chart']}>
+			<h3 className={styles['Chart__title']}>{title}</h3>
+			<Pie data={data} />
+		</div>
+	)
 }
